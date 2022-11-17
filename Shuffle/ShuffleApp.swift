21d9d8ad530ae7f06029847f350f951d2378309a
xeynyty <introwwert@gmail.com>
2022-11-17 07:52:33 +0300

    Initial Commit

---
+++ Shuffle/ShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuffleApp.swift
+//  Shuffle
+//
+//  Created by Влад Кеиг on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
